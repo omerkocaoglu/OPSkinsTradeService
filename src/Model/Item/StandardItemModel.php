60a1ebd2e53f5632a1529f483e0b7f296bea4a39
omer <okocaoglu05@gmail.com>
2018-08-31 16:03:35 +0300

    UPDATE StandardItemModel

--- src/Model/Item/StandardItemModel.php
+++ src/Model/Item/StandardItemModel.php
@@ -12,19 +12,45 @@ class StandardItemModel implements NormalizableInterface
     /** @var int */
     public $id = 0;
     /** @var int */
+    public $sku = 0;
+    /** @var int */
+    public $wear = 0;
+    /** @var int */
+    public $pattern_index = 0;
+    /** @var string */
+    public $previews_urls = null;
+    /** @var string */
+    public $eth_inspect = null;
+    /** @var string */
+    public $trade_hold_expires = null;
+    /** @var int */
     public $internal_app_id = 0;
     /** @var string */
+    public $inspect = null;
+    /** @var bool */
+    public $tradable = false;
+    /** @var string */
     public $name = null;
+    /** @var string */
+    public $category = null;
+    /** @var string */
+    public $rarity = null;
+    /** @var string */
+    public $type = null;
+    /** @var string */
+    public $paint_index = null;
     /** @var string */
     public $color = null;
     /** @var string */
     public $image = null;
     /** @var int */
     public $suggested_price = 0;
-    /** @var bool */
-    public $tradable = false;
-    /** @var StandardItemAttributeModel */
-    public $attributes = null;
+    /** @var int */
+    public $suggested_price_floor = 0;
+
+
+//    /** @var StandardItemAttributeModel */
+//    public $attributes = null;
 
     /**
      * @param NormalizationMetadata $normalization_metadata
